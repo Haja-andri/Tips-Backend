1fd88742d5386ec0a59aa6a1cdd08f7c84fdb437
Haja Andriamaro <haja.andri@gmail.com>
2019-07-31 11:16:24 +0200

    upgraded authenticate.js to make sure the received token matches with the worker record in the DB (workers_token)

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Workers = require('../data/models/workersModel')
 
 const jwtKey = process.env.SALT;
 
@@ -12,10 +13,21 @@ function authenticate(req, res, next) {
     // check Authenrization from request header
     const token = req.get('Authorization');
     if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
+    jwt.verify(token, jwtKey, async (err, decoded) => {
         if (err) return res.status(401).json(err);
         req.decoded = decoded;
-        next();
+        // if the token is valid, we also make sure it matches the current worker entry 
+        // in the workers_token db to prevent workers using others token
+        const alreadyAToken = await Workers.findTokenByWorkerId(req.params.id);
+        if(alreadyAToken.length > 0 && token === alreadyAToken[0].token){
+            // the received token matches with the recorded token in the db
+            next();
+        }
+        else {
+            return res.status(401).json({
+                error: 'You must login with the correct credential',
+            });
+        }
     });
     } else {
         return res.status(401).json({
