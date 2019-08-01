6615bc75cfcb196ee28b252bcd2df02d5b2299b6
Haja Andriamaro <haja.andri@gmail.com>
2019-08-01 13:50:48 +0200

    installed all packages related to testing (jest supertest cross-env) and updated package.json

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 // the actual file that load the server
 const server = require('./server');
 // listning port from current environement or 
 // if not present defaulted at 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 
 server.listen(PORT, ()=>{
